248d5aca1a426c741a39f5b46ffc7069fe7bfed3
dlfernando <antiope@gmail.com>
2020-06-11 19:24:15 +0200

    Version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# Photos-carousel-generator
+# Photos carousel generator
 Script automatically generates html page that displays photos carousel based on images found inside a folder.
+
+----------------------------------------------
+TYPES OF IMAGES SUPPORTED:
+----------------------------------------------
+
+.jpg, .png, .bmp
+
+----------------------------------------------
+CONFIGURATION Lines:
+----------------------------------------------
+
+PHOTOS_FOLDER_LOCATION = 'full photos folder location'
+HTML_TEMPLATE_FILE = 'html template that will be used to generate carousel'
+HTML_OUTPUT = 'Output html file generated with carousel'
+
+Example configuration under Windows S.O enviroment:
+
+PHOTOS_FOLDER_LOCATION = '/path/to/images/'
+HTML_TEMPLATE_FILE = '/path/to/template.html'
+HTML_OUTPUT = '/path/to/output.html'
+
+----------------------------------------------
+EXECUTE SCRIPT:
+----------------------------------------------
+Simply execute script through console:
+
+photos.py
+
+Example execution under Windows S.O enviroment:
+
+python.exe photos.py
---
+++ photos.py
@@ -0,0 +1,54 @@
+import os
+import sys
+
+PHOTOS_FOLDER_LOCATION = ''
+HTML_TEMPLATE_FILE = 'template.html'
+HTML_OUTPUT = 'output.html'
+
+def validimgextensions(filename):
+    accepted = False
+    if filename.endswith('.jpg') or filename.endswith('.JPG') or filename.endswith('.jpeg') or filename.endswith('.JPEG') or filename.endswith('.png') or filename.endswith('.PNG') or filename.endswith('.bmp') or filename.endswith('.BMP'):
+        accepted = True
+    return accepted
+
+def parse_template(slides, carousel):
+    f=open(HTML_TEMPLATE_FILE, "r")
+    if f.mode == 'r':
+        html =f.read()
+        html = html.replace("%slides%", slides)
+        htmldone = html.replace("%carousel%", carousel)
+        try:
+            file = open(HTML_OUTPUT,"w")
+            file.write(htmldone)
+            file.close()
+        except ValueError as e :
+            print ("Error msg: ", e)
+
+
+# ---------------------------- Main program --------------------------- #
+
+s1 = s2 = ''
+i = 0
+
+while True:
+    try:
+        photopath = input("Enter full path of photos: ")
+        for f_name in os.listdir(photopath):
+            if validimgextensions(f_name):
+                if(s1==""):
+                    s1 = '<li data-target="#myCarousel" data-slide-to="'+str(i)+'" class="active">'
+                else:
+                    s1 += '<li data-target="#myCarousel" data-slide-to="'+str(i)+'"></li>'
+                if(s2==""):
+                    s2 = '<div class="item active"><img src="'+PHOTOS_FOLDER_LOCATION+f_name+'" style="width:100%;"></div>'
+                else:
+                    s2 += '<div class="item"><img src="'+PHOTOS_FOLDER_LOCATION+f_name+'" style="width:100%;"></div>'
+                print(f_name)
+                i += 1
+        parse_template(s1, s2)
+        s1 = s2 = ''
+        i = 0
+        print(f"HTML file generated with photos carousel in: {HTML_OUTPUT}\n")
+    except ValueError:
+        print("Oops!  That was no valid path.")
+
